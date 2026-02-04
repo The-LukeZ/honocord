526c07d413a5887790cd18a142fde055d4f711ec
The-LukeZ <contact.lukez@proton.me>
2026-02-04 15:46:46 +0100

    feat: refactor handlers and integrate into main application logic

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -1,22 +1,103 @@
 // Test file for handlers if the types do work
 
-import { ComponentHandler, SlashCommandHandler } from "@ctx/handlers";
+import { AnyHandler, ComponentHandler, ContextCommandHandler, SlashCommandHandler } from "@ctx/handlers";
 import { ComponentType } from "discord-api-types/v10";
+import { BaseInteractionContext, ContextCommandType } from "../types";
 
-const commandHandler = new SlashCommandHandler().setName("test").setDescription("A test command");
+interface MyEnv {
+  DISCORD_PUBLIC_KEY: string;
+  DISCORD_TOKEN: string;
+  DISCORD_APPLICATION_ID: string;
+  DATABASE: D1Database;
+}
+
+interface MyVar {
+  variable: string;
+}
+
+type MyContext = BaseInteractionContext<MyEnv, MyVar>;
+
+const commandHandler = new SlashCommandHandler<MyContext>().setName("test").setDescription("A test command");
 
 commandHandler.addHandler(async (ctx) => {
   console.log("Test command executed");
   await ctx.reply({
     content: "Test command executed",
-    components: [],
   });
 });
 
-const buttonHandler = new ComponentHandler("test_button", ComponentType.Button);
+const buttonHandler = new ComponentHandler<MyContext>("test_button", ComponentType.Button);
 
 buttonHandler.addHandler(async (ctx) => {
   if (ctx.isButton()) {
     console.log("Button clicked with custom ID:", ctx.customId);
   }
 });
+
+const userContextCommandHandler = new ContextCommandHandler<MyContext>(ContextCommandType.User)
+  .setName("user_command")
+  .addHandler(async (ctx) => {
+    console.log("User context command executed");
+  });
+
+const messageContextCommandHandler = new ContextCommandHandler<MyContext>(ContextCommandType.Message)
+  .setName("message_command")
+  .addHandler(async (ctx) => {
+    console.log("Message context command executed");
+  });
+
+// all component interaction handlers
+const buttonComponentHandler = new ComponentHandler<MyContext>("some_id", ComponentType.Button).addHandler(async (ctx) => {
+  console.log("Button clicked");
+});
+
+const stringSelectComponentHandler = new ComponentHandler<MyContext, ComponentType.StringSelect>(
+  "select_1",
+  ComponentType.StringSelect
+).addHandler(async (ctx) => {
+  console.log("Select menu used with values:", ctx.values);
+});
+
+const userSelectComponentHandler = new ComponentHandler<MyContext, ComponentType.UserSelect>(
+  "user_select",
+  ComponentType.UserSelect
+).addHandler(async (ctx) => {
+  console.log("User select used with values:", ctx.users);
+});
+
+const roleSelectComponentHandler = new ComponentHandler<MyContext, ComponentType.RoleSelect>(
+  "role_select",
+  ComponentType.RoleSelect
+).addHandler(async (ctx) => {
+  console.log("Role select used with values:", ctx.roles);
+});
+
+const mentionableSelectComponentHandler = new ComponentHandler<MyContext, ComponentType.MentionableSelect>(
+  "mentionable_select",
+  ComponentType.MentionableSelect
+).addHandler(async (ctx) => {
+  console.log("Mentionable select used with users:", ctx.users);
+  console.log("Mentionable select used with roles:", ctx.roles);
+});
+
+const channelSelectComponentHandler = new ComponentHandler<MyContext, ComponentType.ChannelSelect>(
+  "channel_select",
+  ComponentType.ChannelSelect
+).addHandler(async (ctx) => {
+  console.log("Channel select used with values:", ctx.channels);
+});
+
+const handlers = [
+  commandHandler,
+  buttonHandler,
+  userContextCommandHandler,
+  messageContextCommandHandler,
+  buttonComponentHandler,
+  stringSelectComponentHandler,
+  userSelectComponentHandler,
+  roleSelectComponentHandler,
+  mentionableSelectComponentHandler,
+  channelSelectComponentHandler,
+];
+
+export { handlers };
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,8 +1,11 @@
-import { ComponentHandler, ContextCommandHandler, SlashCommandHandler } from "@ctx/handlers";
-import { BaseInteractionContext, ContextCommandType, HandlerFunction } from "../types";
-import { ComponentType, MessageFlags } from "discord-api-types/v10";
+import { SlashCommandHandler } from "@ctx/handlers";
+import { BaseInteractionContext, HandlerFunction } from "../types";
+import { MessageFlags } from "discord-api-types/v10";
 import { ContainerBuilder } from "@discordjs/builders";
 import { ChatInputCommandInteraction } from "@ctx/ChatInputInteraction";
+import { handlers } from "./handlers";
+import { Honocord } from "../Honocord";
+import { Hono } from "hono/tiny";
 
 interface MyEnv {
   DISCORD_PUBLIC_KEY: string;
@@ -26,42 +29,8 @@ const testHandle: HandlerFunction<MyContext, ChatInputCommandInteraction> = asyn
 };
 
 new SlashCommandHandler<MyContext>().addHandler(testHandle);
+const bot = new Honocord();
+bot.loadHandlers(...handlers);
 
-new ContextCommandHandler<MyContext>(ContextCommandType.User).setName("user_command").addHandler(async (ctx) => {
-  console.log("User context command executed");
-});
-
-new ContextCommandHandler<MyContext>(ContextCommandType.Message).setName("message_command").addHandler(async (ctx) => {
-  console.log("Message context command executed");
-});
-
-// all component interaction handlers
-new ComponentHandler<MyContext>("some_id", ComponentType.Button).addHandler(async (ctx) => {
-  console.log("Button clicked");
-});
-
-new ComponentHandler<MyContext, ComponentType.StringSelect>("select_1", ComponentType.StringSelect).addHandler(async (ctx) => {
-  console.log("Select menu used with values:", ctx.values);
-});
-
-new ComponentHandler<MyContext, ComponentType.UserSelect>("user_select", ComponentType.UserSelect).addHandler(async (ctx) => {
-  console.log("User select used with values:", ctx.users);
-});
-
-new ComponentHandler<MyContext, ComponentType.RoleSelect>("role_select", ComponentType.RoleSelect).addHandler(async (ctx) => {
-  console.log("Role select used with values:", ctx.roles);
-});
-
-new ComponentHandler<MyContext, ComponentType.MentionableSelect>(
-  "mentionable_select",
-  ComponentType.MentionableSelect
-).addHandler(async (ctx) => {
-  console.log("Mentionable select used with users:", ctx.users);
-  console.log("Mentionable select used with roles:", ctx.roles);
-});
-
-new ComponentHandler<MyContext, ComponentType.ChannelSelect>("channel_select", ComponentType.ChannelSelect).addHandler(
-  async (ctx) => {
-    console.log("Channel select used with values:", ctx.channels);
-  }
-);
+const app = new Hono<{ Bindings: MyEnv; Variables: MyVar }>();
+app.post("/interactions", bot.handle);
