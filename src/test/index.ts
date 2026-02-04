063e4b78b45e2ac97a12157021c70df6fba53a3a
The-LukeZ <contact.lukez@proton.me>
2026-02-04 23:06:01 +0100

    docs: update typings and handlers documentation for improved clarity and type safety

--- src/test/index.ts
+++ src/test/index.ts
@@ -6,6 +6,7 @@ import { ChatInputCommandInteraction } from "@ctx/ChatInputInteraction";
 import { handlers } from "./handlers";
 import { Honocord } from "../Honocord";
 import { Hono } from "hono/tiny";
+import { AutocompleteHelper } from "@utils/Autocomplete";
 
 interface MyEnv {
   DISCORD_PUBLIC_KEY: string;
@@ -28,7 +29,25 @@ const testHandle: HandlerFunction<MyContext, ChatInputCommandInteraction> = asyn
   });
 };
 
-new SlashCommandHandler<MyContext>().addHandler(testHandle);
+new SlashCommandHandler<MyContext>().addHandler(testHandle).addAutocompleteHandler(async (ctx) => {
+  const option = ctx.options.getFocused()!; // autocomplete is triggered on any option - so we need to filter
+  if (option?.name === "option-1") {
+    await ctx.respond([
+      { name: "Option 1", value: "option_1" },
+      { name: "Option 2", value: "option_2" },
+    ]);
+    return;
+  }
+
+  // Autocomplete with integrated class
+  const autocomplete = new AutocompleteHelper(option?.value).setChoices(
+    { name: "Choice 1", value: "choice_1" },
+    { name: "Choice 2", value: "choice_2" },
+    { name: "Choice 3", value: "choice_3" }
+  );
+  return ctx.respond(autocomplete.response(["name", "value"])); // Filter by name and value (name_localizations is also supported)
+});
+
 const bot = new Honocord().use<MyContext>();
 bot.loadHandlers(...handlers);
 
