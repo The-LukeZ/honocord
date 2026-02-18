8217a5236361ebbde8e18f4548642d8570b7d9b3
The-LukeZ <contact.lukez@proton.me>
2026-02-18 13:42:19 +0100

    feat: Update package.json and tsdown.config.ts to include memory-cache and mongo-cache entries

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "src/index.ts",
+  clean: true,
+  outDir: "dist",
+  entry: {
+    index: "src/index.ts",
+    "memory-cache": "src/memory-cache/index.ts",
+    "mongo-cache": "src/mongo-cache/index.ts",
+  },
   alias: {
     "@utils": "src/utils",
     "@ctx": "src/interactions",
     "@resolvers": "src/resolvers",
-    "$types": "src/types",
+    $types: "src/types",
     "@handlers": "src/handlers",
   },
+  sourcemap: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  minify: false,
 });
