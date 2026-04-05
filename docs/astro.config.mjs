4a6fc28ef4d1abf5f319a425c477440b8b01d71b
The-LukeZ <contact.lukez@proton.me>
2026-04-06 00:25:56 +0200

    feat: add site URL and new LLMs section to Astro config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,6 +10,9 @@ import starlightContextualMenu from "starlight-contextual-menu";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
+
+  site: "https://honocord.thelukez.com",
+
   redirects: {
     "/github": {
       destination: "https://github.com/The-LukeZ/honocord",
@@ -92,6 +95,14 @@ export default defineConfig({
           autogenerate: { directory: "guides" },
         },
         {
+          label: "For LLMs",
+          link: "/llms.txt",
+          badge: {
+            text: "New",
+            variant: "tip"
+          }
+        },
+        {
           label: "Built With Honocord",
           slug: "built-with",
         },
@@ -103,6 +114,7 @@ export default defineConfig({
             { label: "Constants", autogenerate: { directory: "reference/constants" } },
             { label: "Types", autogenerate: { directory: "reference/types" } },
           ],
+          collapsed: true,
         },
         {
           label: "Examples",
